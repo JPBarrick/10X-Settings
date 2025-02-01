0bed991cfa3a3d6f9c0c4420861e6cd1051e4813
= <=>
2025-01-31 16:43:33 -0800

    Adding margin change for single-dual panel toggle.

--- python-scripts/JPB.py
+++ python-scripts/JPB.py
@@ -9,18 +9,25 @@ def DuplicatePreprocessedLine():
     N10X.Editor.SetCursorPos((x, y))
 
 #------------------------------------------------------------------------
+MARGIN_SINGLE_COLUMN = "1000"
+MARGIN_DUAL_COLUMN = "100"
+
 def ToggleColumn2():
     if N10X.Editor.GetColumnCount() == 1:
         N10X.Editor.SetColumnCount(2)
         N10X.Editor.ExecuteCommand("DuplicatePanelRight")
+        N10X.Editor.OverrideSetting("MarginWidth", MARGIN_DUAL_COLUMN)
     else:
         N10X.Editor.SetColumnCount(1)
+        N10X.Editor.OverrideSetting("MarginWidth", MARGIN_SINGLE_COLUMN)
         N10X.Editor.CloseFile()
 
 def ToggleColumn2Header():
     if N10X.Editor.GetColumnCount() == 1:
         N10X.Editor.SetColumnCount(2)
         N10X.Editor.ExecuteCommand("CppParser.ToggleSourceHeader")
+        N10X.Editor.OverrideSetting("MarginWidth", MARGIN_DUAL_COLUMN)
     else:
         N10X.Editor.SetColumnCount(1)
+        N10X.Editor.OverrideSetting("MarginWidth", MARGIN_SINGLE_COLUMN)
         N10X.Editor.CloseFile()
